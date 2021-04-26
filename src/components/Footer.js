30f872175c48aa6e4dac2a4983d988ada55f1f9a
Kate Smychok <e.smychok@yandex.ru>
2021-04-26 17:33:35 +0300

    fixes: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,25 @@
 import React from "react";
+import {Route, Switch} from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <p className="footer__copyright">&copy;{` Kate Smychok ${new Date().getFullYear()} Mesto Russia`}</p>
-    </footer>
+  <Switch>
+    <Route exact path="/">
+      <footer className="footer">
+        <p className="footer__copyright">&copy;{` Kate Smychok ${new Date().getFullYear()} Mesto Russia`}</p>
+      </footer>
+    </Route>
+    <Route path="/sign-in">
+      <footer className="footer footer-auth">
+        <p className="footer__copyright">&copy;{` Kate Smychok ${new Date().getFullYear()} Mesto Russia`}</p>
+      </footer>
+    </Route>
+    <Route path="/sign-up">
+      <footer className="footer footer-auth">
+        <p className="footer__copyright">&copy;{` Kate Smychok ${new Date().getFullYear()} Mesto Russia`}</p>
+      </footer>
+    </Route>
+  </Switch>
   )
 }
 
